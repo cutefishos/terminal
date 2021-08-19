7d8a60a6a6a820210f6e9269d1b0cb2850c22900
reionwong <reionwong@gmail.com>
2021-08-19 15:00:22 +0800

    Update translations

--- translations/zh_CN.ts
+++ translations/zh_CN.ts
@@ -4,19 +4,43 @@
 <context>
     <name>ExitPromptDialog</name>
     <message>
-        <location filename="../src/qml/ExitPromptDialog.qml" line="32"/>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="49"/>
         <source>Process is running, are you sure you want to quit?</source>
         <translation>有进程在运行中，您确定要退出吗？</translation>
     </message>
     <message>
-        <location filename="../src/qml/ExitPromptDialog.qml" line="40"/>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="63"/>
         <source>OK</source>
         <translation>确定</translation>
     </message>
     <message>
-        <location filename="../src/qml/ExitPromptDialog.qml" line="48"/>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="57"/>
         <source>Cancel</source>
-        <translation type="unfinished">取消</translation>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>Terminal</name>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="171"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="177"/>
+        <location filename="../src/qml/Terminal.qml" line="191"/>
+        <source>Paste</source>
+        <translation>粘贴</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="197"/>
+        <source>Select All</source>
+        <translation>全选</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="202"/>
+        <source>Open File Manager</source>
+        <translation>打开文件管理器</translation>
     </message>
 </context>
 </TS>
