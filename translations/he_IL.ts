b3e40204a5438956ba6935284baaf69ff1176ad8
bugthug <avivyodfat@protonmail.com>
2021-09-09 06:23:41 +0000

    Translated using Weblate (Hebrew (Israel))
    Currently translated at 92.3% (12 of 13 strings)
    
    Translation: CutefishOS/Terminal
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/terminal/he_IL/

--- translations/he_IL.ts
+++ translations/he_IL.ts
@@ -6,17 +6,17 @@
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="49"/>
         <source>Process is running, are you sure you want to quit?</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">התהליך פועל, אתה בטוח שברצונך לצאת?</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="63"/>
         <source>OK</source>
-        <translation type="unfinished"></translation>
+        <translation>אישור</translation>
     </message>
     <message>
         <location filename="../src/qml/ExitPromptDialog.qml" line="57"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>ביטול</translation>
     </message>
 </context>
 <context>
@@ -24,27 +24,27 @@
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="10"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>הגדרות</translation>
     </message>
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="39"/>
         <source>Font</source>
-        <translation type="unfinished"></translation>
+        <translation>פונט</translation>
     </message>
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="63"/>
         <source>Font Size</source>
-        <translation type="unfinished"></translation>
+        <translation>גודל פונט</translation>
     </message>
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="82"/>
         <source>Transparency</source>
-        <translation type="unfinished"></translation>
+        <translation>שקיפות</translation>
     </message>
     <message>
         <location filename="../src/qml/SettingsDialog.qml" line="101"/>
         <source>Window Blur</source>
-        <translation type="unfinished"></translation>
+        <translation>עמעום חלונות</translation>
     </message>
 </context>
 <context>
@@ -52,28 +52,28 @@
     <message>
         <location filename="../src/qml/Terminal.qml" line="177"/>
         <source>Copy</source>
-        <translation type="unfinished"></translation>
+        <translation>העתק</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="183"/>
         <location filename="../src/qml/Terminal.qml" line="197"/>
         <source>Paste</source>
-        <translation type="unfinished"></translation>
+        <translation>הדבק</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="203"/>
         <source>Select All</source>
-        <translation type="unfinished"></translation>
+        <translation>בחר הכל</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="208"/>
         <source>Open File Manager</source>
-        <translation type="unfinished"></translation>
+        <translation>פתח את מנהל הקבצים</translation>
     </message>
     <message>
         <location filename="../src/qml/Terminal.qml" line="213"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>הגדרות</translation>
     </message>
 </context>
 </TS>
