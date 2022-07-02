f31c43a169b2eeaaa8547c3b41b5ac0d16da5c41
rdev32 <rdevelops32@gmail.com>
2022-07-02 10:18:29 -0500

    Spain spanish support

---
+++ translations/es_ES.ts
@@ -0,0 +1,89 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="es_ES">
+<context>
+    <name>ExitPromptDialog</name>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="49"/>
+        <source>Process is running, are you sure you want to quit?</source>
+        <translation>Un proceso se esta ejecutando ¿Estáis seguro de salir de la aplicación?</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="63"/>
+        <source>OK</source>
+        <translation>Aceptar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/ExitPromptDialog.qml" line="57"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../src/qml/SettingsDialog.qml" line="10"/>
+        <source>Settings</source>
+        <translation type="unfinished">Configuración</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDialog.qml" line="39"/>
+        <source>Font</source>
+        <translation type="unfinished">Fuente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDialog.qml" line="63"/>
+        <source>Font Size</source>
+        <translation type="unfinished">Tamaño de la Fuente</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDialog.qml" line="82"/>
+        <source>Transparency</source>
+        <translation type="unfinished">Transparencia</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsDialog.qml" line="101"/>
+        <source>Window Blur</source>
+        <translation type="unfinished">Difuminado de Pantalla</translation>
+    </message>
+</context>
+<context>
+    <name>Terminal</name>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="178"/>
+        <source>Copy</source>
+        <translation type="unfinished">Copiar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="184"/>
+        <location filename="../src/qml/Terminal.qml" line="198"/>
+        <source>Paste</source>
+        <translation type="unfinished">Pegar</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="204"/>
+        <source>Select All</source>
+        <translation type="unfinished">Seleccionar todo</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="209"/>
+        <source>Open File Manager</source>
+        <translation type="unfinished">Abrir Explorador de Archivos</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="214"/>
+        <source>Exit full screen</source>
+        <translation type="unfinished">Cerrar pantalla completa</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="214"/>
+        <source>Full screen</source>
+        <translation type="unfinished">Pantalla completa</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/Terminal.qml" line="221"/>
+        <source>Settings</source>
+        <translation type="unfinished">Configuración</translation>
+    </message>
+</context>
+</TS>
